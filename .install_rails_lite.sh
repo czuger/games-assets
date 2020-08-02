c929863e503059c6258e362083c3fd27c31bf52a
ced <zuger.cedric@gmail.com>
2020-08-02 16:37:14 +0200

    New rails version without webpacker.

---
+++ .install_rails_lite.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rails new games-assets -d sqlite3 -JMC --skip-action-text --skip-active-storage --skip-bootsnap --skip
---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,6 @@
+class PhotosController < ApplicationController
+
+  def new
+  end
+
+end
